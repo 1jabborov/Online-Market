fb9b60876c7b4e39e3c6da8f36ad2f740bd0fefd
1jabborov <1jabborovv@gmail.com>
2023-06-08 08:40:23 +0500

    Change user app

--- user/validators.py
+++ user/validators.py
@@ -1,5 +1,5 @@
 def phone_validator(value):
     if not len(value) == 13:
-        raise ValueError("Telefon raqamida xatolik bor to'g'ri tel kriting misol +998882640011")
+        raise ValueError("There is an error in the phone number, enter the correct phone number, for example: +998882640011")
     else:
         return True
